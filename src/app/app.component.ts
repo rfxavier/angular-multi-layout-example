1ef56e4f14f71ac35d16e90ad132a378e3743c1d
Renato Xavier <you@example.com>
2018-12-26 17:54:37 -0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: []
+})
+export class AppComponent  {
+  name = 'Angular 4';
+  isCollapsed = true;
+}
